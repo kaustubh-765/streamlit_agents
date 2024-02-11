b77c76bc746157af0b46a8e2ca2eca8f6b125601
Kaustubh <90597818+kaustubh-765@users.noreply.github.com>
2024-02-11 15:27:03 +0530

    Update coding_problem_generator.py

--- coding_problem_generator.py
+++ coding_problem_generator.py
@@ -1,7 +1,6 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from dotenv import load_dotenv
 import os 
 import json
 from pydantic import BaseModel
